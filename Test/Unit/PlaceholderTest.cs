7b8f86cd97a5169182820596883a78046aa0b5d7
filip-wojc <filipwojcik203@gmail.com>
2025-09-16 11:21:58 +0200

    create test project with needed dependencies

---
+++ Test/Unit/PlaceholderTest.cs
@@ -0,0 +1,14 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Test.Unit;
+
+public class PlaceholderTest
+{
+    [Fact]
+    public void Test()
+    {
+        int result = 5 * 3;
+        result.Should().Be(15);
+    }
+}
