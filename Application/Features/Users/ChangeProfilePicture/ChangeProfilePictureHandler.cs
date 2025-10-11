63d8bbdd32d6f15069530c830d3cb510afb4f540
filip-wojc <filipwojcik203@gmail.com>
2025-10-11 18:04:44 +0200

    use transaction in ChangeProfilePictureHandler.cs

--- Application/Features/Users/ChangeProfilePicture/ChangeProfilePictureHandler.cs
+++ Application/Features/Users/ChangeProfilePicture/ChangeProfilePictureHandler.cs
@@ -5,6 +5,7 @@ using Infrastructure.Interfaces.Repositories;
 using Infrastructure.Interfaces.Services;
 using MediatR;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
 
 namespace Application.Features.Users.ChangeProfilePicture;
 
@@ -12,8 +13,9 @@ public class ChangeProfilePictureHandler(
     IUserRepository userRepository,
     IPhotoRepository photoRepository,
     IUnitOfWork unitOfWork,
-    IMediaService mediaService
-) : IRequestHandler<ChangeProfilePictureCommand, string>
+    IMediaService mediaService,
+    ILogger<ChangeProfilePictureHandler> logger
+    ) : IRequestHandler<ChangeProfilePictureCommand, string>
 {
     public async Task<string> Handle(ChangeProfilePictureCommand request, CancellationToken cancellationToken)
     {
@@ -32,34 +34,51 @@ public class ChangeProfilePictureHandler(
         if (existingUser == null)
             throw new NotFoundException("User not found");
 
-        var uploadResult = await mediaService.UploadPhotoAsync(request.File);
-        if (uploadResult.Error != null)
-            throw new CloudinaryException(uploadResult.Error.Message);
-
-        // remove old photo
-        if (existingUser.ProfilePhoto != null)
+        await unitOfWork.BeginTransactionAsync();
+        string? uploadPhotoPublicId = null;
+        try
         {
-            var photoId = existingUser.ProfilePhoto.Id;
-            var deletionResult = await mediaService.DeleteImageAsync(existingUser.ProfilePhoto.PublicId);
-            if (deletionResult.Error != null)
-                throw new CloudinaryException(deletionResult.Error.Message);
+            var uploadResult = await mediaService.UploadPhotoAsync(request.File);
+            if (uploadResult.Error != null)
+                throw new CloudinaryException(uploadResult.Error.Message);
+            
+            uploadPhotoPublicId = uploadResult.PublicId;
+            
+            // remove old photo
+            if (existingUser.ProfilePhoto != null)
+            {
+                var oldPhoto = existingUser.ProfilePhoto;
+                var deletionResult = await mediaService.DeleteImageAsync(oldPhoto.PublicId);
+                if (deletionResult.Error != null)
+                    throw new CloudinaryException(deletionResult.Error.Message);
 
-            await userRepository.RemoveProfilePhotoAsync(existingUser.Id); // removes the reference to photo
-            await photoRepository.DeletePhotoAsync(photoId); // removes photo row from db
-            await unitOfWork.SaveChangesAsync(cancellationToken);
+                await userRepository.RemoveProfilePhotoAsync(existingUser.Id); // removes the reference to photo
+                await photoRepository.DeletePhotoAsync(oldPhoto.Id); // removes photo row from db
+            }
+
+            var photo = new Photo
+            {
+                Url = uploadResult.SecureUrl.AbsoluteUri,
+                PublicId = uploadResult.PublicId,
+                DateUploaded = DateTime.UtcNow,
+            };
+
+            await photoRepository.AddPhotoAsync(photo);
+            existingUser.ProfilePhoto = photo;
+            await unitOfWork.CommitTransactionAsync();
+
+            return photo.Url;
         }
-
-        var photo = new Photo
+        catch (Exception ex)
         {
-            Url = uploadResult.SecureUrl.AbsoluteUri,
-            PublicId = uploadResult.PublicId,
-            DateUploaded = DateTime.UtcNow,
-        };
+            logger.LogError(ex, $"Error when changing profile photo: {ex.Message}");
+            await unitOfWork.RollbackTransactionAsync();
+            if (uploadPhotoPublicId != null)
+            {
+                await mediaService.DeleteImageAsync(uploadPhotoPublicId);
+            }
 
-        await photoRepository.AddPhotoAsync(photo);
-        existingUser.ProfilePhoto = photo;
-        await unitOfWork.SaveChangesAsync(cancellationToken);
-
-        return photo.Url;
+            throw;
+        }
     }
 }
