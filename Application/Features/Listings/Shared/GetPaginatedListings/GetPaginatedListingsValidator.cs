28416a8779878516842e09c8348f03e4b0d64a0f
RobertPintera <robert.it.990@gmail.com>
2025-11-10 17:29:24 +0100

    Fix price validation rules for GetPaginatedListings

--- Application/Features/Listings/Shared/GetPaginatedListings/GetPaginatedListingsValidator.cs
+++ Application/Features/Listings/Shared/GetPaginatedListings/GetPaginatedListingsValidator.cs
@@ -7,7 +7,13 @@ public class GetPaginatedListingsValidator : AbstractValidator<GetPaginatedListi
     public GetPaginatedListingsValidator()
     {
         RuleFor(q => q.PageSize).GreaterThan(0).LessThanOrEqualTo(100);
-        RuleFor(q => q.PriceFrom).GreaterThan(0).LessThanOrEqualTo(q => q.PriceTo);
-        RuleFor(q => q.PriceTo).GreaterThan(q => q.PriceFrom);
+        RuleFor(q => q.PriceFrom)
+            .GreaterThan(0)
+            .LessThanOrEqualTo(q => q.PriceTo ?? decimal.MaxValue)
+            .When(q => q.PriceFrom.HasValue);
+
+        RuleFor(q => q.PriceTo)
+            .GreaterThanOrEqualTo(q => q.PriceFrom ?? 0)
+            .When(q => q.PriceTo.HasValue);
     }
 }
