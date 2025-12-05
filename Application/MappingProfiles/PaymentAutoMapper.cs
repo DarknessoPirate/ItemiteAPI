62b45f8313bb0e1a6bb5485e5ad4249f3d6afd87
DarknessoPirate <darknesso539@gmail.com>
2025-12-05 20:26:00 +0100

    added entities for seller and buyer payments dtos

--- Application/MappingProfiles/PaymentAutoMapper.cs
+++ Application/MappingProfiles/PaymentAutoMapper.cs
@@ -6,7 +6,7 @@ namespace Application.MappingProfiles;
 
 public class PaymentAutoMapper : Profile
 {
-    public PaymentAutoMapper() 
+    public PaymentAutoMapper()
     {
         CreateMap<Payment, PaymentResponse>()
             .ForMember(dest => dest.PaymentId, opt =>
@@ -18,6 +18,23 @@ public class PaymentAutoMapper : Profile
             .ForMember(dest => dest.Seller, opt =>
                 opt.MapFrom(src => src.Seller))
             .ForMember(dest => dest.ApprovedBy, opt =>
-                opt.MapFrom(src => src.ApprovedBy));
+                opt.MapFrom(src => src.ApprovedBy))
+            .ForMember(dest => dest.Dispute, opt =>
+                opt.MapFrom(src => src.Dispute));
+
+
+        CreateMap<Payment, PaymentBuyerResponse>()
+            .ForMember(dest => dest.PaymentId, opt => opt.MapFrom(src => src.Id))
+            .ForMember(dest => dest.Listing, opt => opt.MapFrom(src => src.Listing))
+            .ForMember(dest => dest.Buyer, opt => opt.MapFrom(src => src.Buyer))
+            .ForMember(dest => dest.Seller, opt => opt.MapFrom(src => src.Seller))
+            .ForMember(dest => dest.Dispute, opt => opt.MapFrom(src => src.Dispute));
+        
+        CreateMap<Payment, PaymentSellerResponse>()
+            .ForMember(dest => dest.PaymentId, opt => opt.MapFrom(src => src.Id))
+            .ForMember(dest => dest.Listing, opt => opt.MapFrom(src => src.Listing))
+            .ForMember(dest => dest.Buyer, opt => opt.MapFrom(src => src.Buyer))
+            .ForMember(dest => dest.Seller, opt => opt.MapFrom(src => src.Seller));
+
     }
 }
